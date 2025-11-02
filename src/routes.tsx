f401f35a6880051875e4e6fd3ba05667ce2c905a
Kamogelo <ekmogasoa@gmail.com>
2025-11-02 11:38:43 +0200

    fixed homepage route path

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Homepage from "./feature/pages/home/homepage";
+import Homepage from "./feature/pages/home/homePage";
 import PageErrorRedirect from "./feature/pages/pageNotFound/PageErrorRedirect";
 
 const Routespath = () => {
