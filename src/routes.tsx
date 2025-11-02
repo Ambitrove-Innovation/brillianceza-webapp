06bf8da3710b7d82344d73856d31ff790cbe92ce
Kamogelo <ekmogasoa@gmail.com>
2025-11-02 12:53:25 +0200

    implemented page components and type declarations for form and products

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,20 @@
+// src/routes.tsx
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./feature/pages/home/homePage";
+import AboutPage from "./feature/pages/about/aboutPage";
+import ShopPage from "./feature/pages/shop/shopPage";
 import PageErrorRedirect from "./feature/pages/pageNotFound/PageErrorRedirect";
 
 const Routespath = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="*" element={<PageErrorRedirect />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="*" element={<PageErrorRedirect />} />
+      </Routes>
+    </Router>
   );
 };
 
