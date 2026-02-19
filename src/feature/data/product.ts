91fecc517683b8a20042e012fc4313242124a7c2
Meyer.dev <meyerdean.developer@gmail.com>
2026-02-19 18:18:58 +0200

    Merge remote main with local repo

--- src/feature/data/product.ts
+++ src/feature/data/product.ts
@@ -1,6 +1,5 @@
 export const products: Product[] = [
   {
-<<<<<<< HEAD
     id: "gum-elastic-wte",
     name: "Gum Elastic W.T.E. T-SHIRT",
     price: 349.0,
@@ -13,8 +12,6 @@ export const products: Product[] = [
       "Customers love its oversize fit, giving Streetwear style all the way.",
   },
   {
-=======
->>>>>>> 4420d74ebc785c8601c338f09c8ab5bd495a93ce
     id: "3bs-tshirt",
     name: "3 B'S T-SHIRT",
     price: 279.0,
